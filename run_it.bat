2a54493092e85d576de87c2df4045c358dc44370
nirnanaaa <mosny@zyg.li>
2014-11-18 13:11:23 +0100

    added second vm

---
+++ run_it.bat
@@ -0,0 +1,9 @@
+rem VBOX script
+
+
+set VBOX_HEADLESS_PATH="C:\Program Files\Oracle\VirtualBox\VBoxHeadless.exe"
+set VMNAME=u64_it
+
+%VBOX_HEADLESS_PATH% -s %VMNAME%  -v on
+
+
--- vm.run.vbs
+++ vm.run.vbs
@@ -1,3 +1,4 @@
 Set WshShell = WScript.CreateObject("WScript.Shell")
 obj = WshShell.Run("run_dev.bat", 0)
+obj2 = WshShell.Run("run_it.bat",0)
 set WshShell = Nothing
